d9571dd6791506edd3ac727d1ebddbcd95049853
Richard Stiehm <richardstiehm@gmail.com>
2015-10-05 17:15:55 -0400

    First round of hunt the wumpus

---
+++ chap_1.py
@@ -0,0 +1,30 @@
+from random import choice
+
+cave_numbers = range(1, 21)
+wumpus_location = choice(cave_numbers)
+player_location = choice(cave_numbers)
+while player_location == wumpus_location:
+    player_location = choice(cave_numbers)
+
+print "\nWelcome to Hunt The Wumpus!!!"
+print "From where you stand\nYou can see {} caves".format(len(cave_numbers))
+print "To start playing, enter a number\nfor the cave you wish to enter next"
+
+
+while True:
+    print "You are in cave {} presently" .format(player_location)
+    if (player_location == wumpus_location -1 or
+        player_location == wumpus_location + 1):
+        print "I smell a wumpus!!!"
+
+    print "Which cave next?"
+    player_input = raw_input(">")
+    if (not player_input.isdigit() or
+        int(player_input) not in cave_numbers):
+        print "{} is not a cave!!!".format(player_input)
+
+    else:
+        player_location = int(player_input)
+        if player_location == wumpus_location:
+            print "Arrgh you got eaten by the wumpus!!!!"
+            break
